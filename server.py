87a57a73caebeb308846389d3063d69d1fbca18a
EZ-ZAHRAOUI-FS <0ezzahraouisalaheddine@gmail.com>
2025-05-25 20:32:29 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+
+PORT = 8000
+DIRECTORY = "www.com"
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+if __name__ == "__main__":
+    with TCPServer(("", PORT), MyHandler) as httpd:
+        print(f"Server started at http://localhost:{PORT}")
+        print("you are so SMART ID12")
+        httpd.serve_forever()
